1c14aa1a39717e7aab90f8a3f362c47730be5d8f
ericmw <m.ericmwangi@gmail.com>
2019-02-25 09:47:14 +0300

    first commit for this blog

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="/lsapp/public">{{config('app.name','LSAPP')}}</a>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/lsapp/public">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/lsapp/public/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/lsapp/public/services">Services</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
